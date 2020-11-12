20ece5471aee5e0543d88782ac289b552d8aa10e
Yujie Wu <allenwuyujie@gmail.com>
2020-11-13 02:03:37 +1100

    add styles into note section

--- src/pages/budget/budget.page.tsx
+++ src/pages/budget/budget.page.tsx
@@ -27,7 +27,26 @@ const TagListContainer = styled.ol`
   }
 `;
 
-const NotesSection = styled.section``;
+const NotesSection = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label {
+    display: flex;
+    align-items: center;
+    > span {
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+    > input {
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+`;
 const CategoriesSection = styled.section``;
 const NumberPadSection = styled.section``;
 
@@ -46,7 +65,7 @@ function Budget() {
       <NotesSection>
         <label>
           <span>Description</span>
-          <input type="text" />
+          <input type="text" placeholder='describe your new transaction here' />
         </label>
       </NotesSection>
       <CategoriesSection>
