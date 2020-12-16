9d8af5e4cf3330a227bad5ccd84e11f70125cc73
Yujie Wu <allenwuyujie@gmail.com>
2020-12-17 01:48:39 +1100

    add script for deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+yarn build &&
+cd build &&
+git init &&
+git remote add origin https://github.com/allenwyj/react-budgety-app-website.git &&
+git add . &&
+git commit -m 'deploy' &&
+git branch -M main &&
+git push -u origin master -f
+cd ..
