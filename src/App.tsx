999c825f054f22ba5e0ec33c9c32ba01040a62b2
Yujie Wu <allenwuyujie@gmail.com>
2020-12-17 12:33:02 +1100

    removed HomePage component, and changed Budget page as the home page

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import {
 import styled from 'styled-components';
 import NoMatch from './pages/404-page/no-match.page';
 import Budget from './pages/budget-page/budget.page';
-import HomePage from './pages/home-page/home.page';
 import Statistics from './pages/statistics-page/statistic.page';
 import TagPage from './pages/tag-page/tag.page';
 import TagsCollection from './pages/tags-collection/tags-collection.page';
@@ -22,8 +21,7 @@ function App() {
     <AppWrapper>
       <Router>
         <Switch>
-          <Redirect exact from="/" to="/home" />
-          <Route exact path="/home" component={HomePage} />
+          <Redirect exact from="/" to="/budget" />
           <Route exact path="/tags" component={TagsCollection} />
           <Route exact path="/tags/:id" component={TagPage} />
           <Route exact path="/budget" component={Budget} />
