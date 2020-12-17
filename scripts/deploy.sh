a2d8d4680e7ba66450416b83238c1889edd52a4b
Yujie Wu <allenwuyujie@gmail.com>
2020-12-17 15:09:11 +1100

    udpate deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ cd build &&
 git init &&
 git remote add origin https://github.com/allenwyj/react-budgety-app-website.git &&
 git add . &&
-git commit -m 'deploy' &&
+git commit -m "deploy" &&
 git branch -M main &&
-git push -u origin master -f
+git push -u origin main -f
 cd ..
